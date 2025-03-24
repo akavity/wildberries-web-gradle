431de201f0c0263b69a8c1701f4519a7d322c6ba
Basil Akavity <vakavity@gmail.com>
2025-03-24 18:40:57 +0300

    parameterizeTest added

--- src/main/java/org/akavity/utils/JsonReader.java
+++ src/main/java/org/akavity/utils/JsonReader.java
@@ -4,34 +4,40 @@ import com.google.gson.Gson;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 import org.akavity.annotations.TestData;
-import org.testng.annotations.DataProvider;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Stream;
 
-public class JsonReader {
-    @DataProvider
-    public Object[][] getData(Method method) throws FileNotFoundException {
+public class JsonReader implements ArgumentsProvider {
+    @Override
+    public Stream<? extends Arguments> provideArguments(org.junit.jupiter.api.extension.ExtensionContext context) throws Exception {
+        Method testMethod = context.getTestMethod().orElseThrow(() -> new RuntimeException("No test method found"));
+        TestData testDataAnnotation = testMethod.getAnnotation(TestData.class);
+
+        if (testDataAnnotation == null) {
+            throw new RuntimeException("TestData annotation is missing on method: " + testMethod.getName());
+        }
+
         String jsonData = JsonParser.parseReader(new FileReader("src/test/resources/test-data/"
-                + method.getAnnotation(TestData.class).folder() + "/"
-                + method.getAnnotation(TestData.class).jsonFile() + ".json")).toString();
+                + testDataAnnotation.folder() + "/"
+                + testDataAnnotation.jsonFile() + ".json")).toString();
 
-        ArrayList<Object> list = null;
+        ArrayList<Object> list;
         try {
             list = new Gson().fromJson(jsonData, TypeToken.getParameterized(List.class,
-                    Class.forName("org.akavity.models." + method.getAnnotation(TestData.class).folder()
-                            + "." + method.getAnnotation(TestData.class)
-                            .model())).getType());
+                    Class.forName("org.akavity.models." + testDataAnnotation.folder()
+                            + "." + testDataAnnotation.model())).getType());
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Model class not found", e);
         }
 
         return Objects.requireNonNull(list).stream()
-                .map(data -> new Object[]{data})
-                .toArray(Object[][]::new);
+                .map(Arguments::of);
     }
 }
--- src/test/java/org/akavity/tests/ProductTest.java
+++ src/test/java/org/akavity/tests/ProductTest.java
@@ -4,8 +4,12 @@ import org.akavity.annotations.TestData;
 import org.akavity.models.productTest.SearchData;
 import org.akavity.steps.*;
 import org.akavity.utils.JsonReader;
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
 
 public class ProductTest extends BaseTest {
     CatalogSteps catalogSteps = new CatalogSteps();
@@ -15,33 +19,38 @@ public class ProductTest extends BaseTest {
     HeaderSteps headerSteps = new HeaderSteps();
     BasketSteps basketSteps = new BasketSteps();
 
-    @TestData(jsonFile = "searchData", model = "SearchData", folder = "productTest")
-    @Test(description = "Search by product name", dataProviderClass = JsonReader.class, dataProvider = "getData")
+    @ParameterizedTest
+    @ArgumentsSource(JsonReader.class)
+    @TestData(folder = "productTest", jsonFile = "searchData", model = "SearchData")
+    @DisplayName("Search by product name")
     public void productSearch(SearchData searchData) {
         headerSteps.enterTextInSearchField(searchData.getText());
         headerSteps.clickLoupeButton();
 
-        Assert.assertTrue(catalogSteps.doProductNamesContainText(searchData.getText()));
+        Assertions.assertTrue(catalogSteps.doProductNamesContainText(searchData.getText()));
     }
 
-    @Test(description = "View reviews")
+    @Test
+    @DisplayName("View reviews")
     public void viewReviews() {
         catalogSteps.selectFirstProductCard();
         productSteps.clickViewAllCommentsButton();
 
-        Assert.assertTrue(userActivitySteps.isFeedbackButtonDisplayed());
+        Assertions.assertTrue(userActivitySteps.isFeedbackButtonDisplayed());
     }
 
-    @Test(description = "View Questions")
+    @Test
+    @DisplayName("View Questions")
     public void viewQuestions() {
         catalogSteps.selectFirstProductCard();
         productSteps.clickQuestionsButton();
         productSteps.clickViewAllQuestionButton();
 
-        Assert.assertTrue(userActivitySteps.isQuestionTitleDisplayed());
+        Assertions.assertTrue(userActivitySteps.isQuestionTitleDisplayed());
     }
 
-    @Test(description = "Open product popup")
+    @Test
+    @DisplayName("Open product popup")
     public void openProductPopup() {
         String cardBrand = catalogSteps.getFirstProductCardBrand();
         double cardPrice = catalogSteps.getFirstProductCardPrice();
@@ -49,12 +58,13 @@ public class ProductTest extends BaseTest {
         String popupBrand = popupSteps.getProductBrand();
         double popupPrice = popupSteps.getProductPrice();
 
-        Assert.assertTrue(popupSteps.isPopupBlockDisplayed());
-        Assert.assertEquals(cardBrand, popupBrand);
-        Assert.assertEquals(cardPrice, popupPrice);
+        Assertions.assertTrue(popupSteps.isPopupBlockDisplayed());
+        Assertions.assertEquals(cardBrand, popupBrand);
+        Assertions.assertEquals(cardPrice, popupPrice);
     }
 
-    @Test(description = "Add product to basket")
+    @Test
+    @DisplayName("Add product to basket")
     public void addProductToBasket() {
         double cardPrice = catalogSteps.getFirstProductCardPrice();
         catalogSteps.clickFirstButtonAddToBasket();
@@ -62,6 +72,6 @@ public class ProductTest extends BaseTest {
         headerSteps.clickBasketButton();
         double basketPrice = basketSteps.getFinalPrice();
 
-        Assert.assertEquals(cardPrice, basketPrice);
+        Assertions.assertEquals(cardPrice, basketPrice);
     }
 }
