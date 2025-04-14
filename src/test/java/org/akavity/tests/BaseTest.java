d0a475cec2b6327b06bba6ea7b25d4b9c92a1aed
Basil Akavity <vakavity@gmail.com>
2025-04-14 20:09:30 +0300

    thread local has been removed

--- src/test/java/org/akavity/tests/BaseTest.java
+++ src/test/java/org/akavity/tests/BaseTest.java
@@ -23,14 +23,10 @@ import static com.codeborne.selenide.Selenide.*;
 @Execution(ExecutionMode.CONCURRENT)
 public class BaseTest {
     private static final ResourceBundle BUNDLE = ResourceBundle.getBundle("test_framework");
-    private static final ThreadLocal<Boolean> initialized = ThreadLocal.withInitial(() -> false);
 
     @BeforeEach
     public void setUp() {
-        if (!initialized.get()) {
-            startBrowser();
-            initialized.set(true);
-        }
+        startBrowser();
         open(BUNDLE.getString("path_to_url"));
     }
 
@@ -39,10 +35,9 @@ public class BaseTest {
         clearBrowserLocalStorage();
         clearBrowserCookies();
         closeWebDriver();
-        initialized.set(false);
     }
 
-    private synchronized void startBrowser() {
+    private void startBrowser() {
         String startType = System.getProperty("startType", BUNDLE.getString("test.startType"));
         String browser = System.getProperty("browser", BUNDLE.getString("test.browser"));
         String version = System.getProperty("version", BUNDLE.getString("test.version"));
@@ -61,6 +56,7 @@ public class BaseTest {
                 .enableLogs(LogType.BROWSER, Level.ALL));
         Configuration.browserSize = "1920x1080";
         Configuration.browser = CHROME;
+        Configuration.timeout = 8_000;
     }
 
     private static void startSelenoid(String browser, String version) {
@@ -71,6 +67,7 @@ public class BaseTest {
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "http://localhost:4444/wd/hub";
         Configuration.browser = browser;
+        Configuration.timeout = 8_000;
 
         Map<String, Boolean> options = new HashMap<>();
         options.put("enableVNC", true);
